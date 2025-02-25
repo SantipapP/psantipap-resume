efe9bcf1cc3b6ba687c5e9e88dc0f7e41bd7fd5b
SantipapP <89773041+SantipapP@users.noreply.github.com>
2025-02-25 21:41:16 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import Header from "./components/Header";
 import About from "./components/About";
 import Skill from "./components/Skill";
