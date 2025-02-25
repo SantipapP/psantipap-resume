9db78d78509396924b080a60f3e2d9146bd4425d
SantipapP <89773041+SantipapP@users.noreply.github.com>
2025-02-25 22:24:36 +0700

    Update Project.tsx

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Link from 'next/link'
 function Project() {
     return (
         <>
@@ -7,14 +7,12 @@ function Project() {
                 <div className="max-w-4xl mx-auto">
                     <h2 className="text-3xl font-bold mb-4">Projects</h2>
                     <div className="space-y-6">
-                        <div className="bg-white p-6 shadow rounded-lg hover:scale-105 transition-transform duration-300">
-                            <h3 className="text-xl font-semibold">Project 1: E-commerce Website</h3>
-                            <p>Developed a full-stack e-commerce platform using Next.js and MongoDB.</p>
-                        </div>
-                        <div className="bg-white p-6 shadow rounded-lg hover:scale-105 transition-transform duration-300">
-                            <h3 className="text-xl font-semibold">Project 2: Task Management App</h3>
-                            <p>Created a task management web app with React and Firebase.</p>
-                        </div>
+                        <Link href="https://pokemon-finder-gilt.vercel.app/" target='_blank'>
+                            <div className="bg-white p-6 shadow rounded-lg hover:scale-105 transition-transform duration-300">
+                                <h3 className="text-xl font-semibold">Project 1: Pokemon Finder App</h3>
+                                <p>Developed a web app for searching and displaying Pokémon information using Next.js and Tailwind CSS.</p>
+                            </div>
+                        </Link>
                     </div>
                 </div>
             </section>
