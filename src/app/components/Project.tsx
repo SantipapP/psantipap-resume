8aedf1f6435b6afc27ebfa4a615c80e250b89a57
SantipapP <89773041+SantipapP@users.noreply.github.com>
2025-03-02 22:12:40 +0700

    change url project

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -7,13 +7,13 @@ function Project() {
                 <div className="max-w-4xl mx-auto">
                     <h2 className="text-3xl font-bold mb-4">Projects</h2>
                     <div className="space-y-6">
-                        <Link href="https://pokemon-finder-gilt.vercel.app/" target='_blank'>
+                        <Link href="https://github.com/SantipapP/PokemonFinder" target='_blank'>
                             <div className="bg-white p-6 shadow rounded-lg hover:scale-105 transition-transform duration-300 m-4">
                                 <h3 className="text-xl font-semibold">Project 1: Pokemon Finder App</h3>
                                 <p>Developed a web app for searching and displaying Pokémon information using Next.js and Tailwind CSS.</p>
                             </div>
                         </Link>
-                        <Link href="https://hrm-system-gamma.vercel.app/" target='_blank'>
+                        <Link href="https://github.com/SantipapP/hrm-system" target='_blank'>
                             <div className="bg-white p-6 shadow rounded-lg hover:scale-105 transition-transform duration-300 m-4">
                                 <h3 className="text-xl font-semibold">Project 2: HRM System App</h3>
                                 <p>Developed a web app to support HR staff operations.</p>
