cb75e0fb7087a6662cc0d31412898bc3184a990a
SantipapP <89773041+SantipapP@users.noreply.github.com>
2025-03-02 22:10:13 +0700

    add margin in Project div

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -8,13 +8,13 @@ function Project() {
                     <h2 className="text-3xl font-bold mb-4">Projects</h2>
                     <div className="space-y-6">
                         <Link href="https://pokemon-finder-gilt.vercel.app/" target='_blank'>
-                            <div className="bg-white p-6 shadow rounded-lg hover:scale-105 transition-transform duration-300">
+                            <div className="bg-white p-6 shadow rounded-lg hover:scale-105 transition-transform duration-300 m-4">
                                 <h3 className="text-xl font-semibold">Project 1: Pokemon Finder App</h3>
                                 <p>Developed a web app for searching and displaying Pokémon information using Next.js and Tailwind CSS.</p>
                             </div>
                         </Link>
                         <Link href="https://hrm-system-gamma.vercel.app/" target='_blank'>
-                            <div className="bg-white p-6 shadow rounded-lg hover:scale-105 transition-transform duration-300">
+                            <div className="bg-white p-6 shadow rounded-lg hover:scale-105 transition-transform duration-300 m-4">
                                 <h3 className="text-xl font-semibold">Project 2: HRM System App</h3>
                                 <p>Developed a web app to support HR staff operations.</p>
                             </div>
